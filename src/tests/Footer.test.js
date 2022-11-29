8ada9a6466e3e9cb0b2b8db4c87585d9e2af8245
joao <joaopaz79@outlook.com>
2022-11-29 20:59:18 -0300

    Refatora requisito 18

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,31 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
+import renderWithRouter from '../Helpers/renderWithRouter';
 import Recipes from '../pages/Recipes';
 
 describe('Testa o componente Footer', () => {
   test('Testa se tudo é renderizado como esperado', () => {
-    render(<Recipes />);
+    renderWithRouter(<Recipes />);
     expect(screen.getByTestId('meals-bottom-btn')).toBeInTheDocument();
     expect(screen.getByTestId('drinks-bottom-btn')).toBeInTheDocument();
   });
   test('Testa se o botão meals direcionapara o local certo', () => {
-    render(
-      <BrowserRouter>
-        <Recipes />
-      </BrowserRouter>,
-    );
+    const { history } = renderWithRouter(<Recipes />);
     const meals = screen.getByTestId('meals-bottom-btn');
     userEvent.click(meals);
+    expect(history.location.pathname).toBe('/meals');
   });
 
   test('Testa se o botão drinks direcionapara o local certo', () => {
-    render(
-      <BrowserRouter>
-        <Recipes />
-      </BrowserRouter>,
-    );
+    const { history } = renderWithRouter(<Recipes />);
     const drinks = screen.getByTestId('drinks-bottom-btn');
     userEvent.click(drinks);
+    expect(history.location.pathname).toBe('/drinks');
   });
 });
