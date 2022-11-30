fbe19fee6375a3b0118210aff63cebfa45363c7d
Robson Aires <robsonaires@outlook.com.br>
2022-11-30 19:54:22 -0300

    group-10/req23-24

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-// import { useHistory } from 'react-router-dom';
 import recipesContext from '../context/recipesContext';
 import { apiIngredientes } from '../services/apiDrinks';
 
 function Drinks() {
+  const history = useHistory();
   const number12 = 12;
   const { data, setData, setLoading, loading } = useContext(recipesContext);
   useEffect(() => {
@@ -14,6 +15,11 @@ function Drinks() {
     setLoading(false);
   }, [setLoading, setData]);
   if (loading) return <p>Carregando...</p>;
+
+  const handleSubmit = (target) => {
+    const result = target;
+    history.push(`/drinks/${result}`);
+  };
   return (
     <div>
       <Header title="Drinks" />
@@ -26,10 +32,12 @@ function Drinks() {
                key={ index }
              >
                <p data-testid={ `${index}-card-name` }>{Ingredient.strDrink}</p>
-               <img
-                 src={ Ingredient.strDrinkThumb }
-                 alt="asdfgh"
+               <input
                  data-testid={ `${index}-card-img` }
+                 type="image"
+                 alt="asd"
+                 src={ Ingredient.strDrinkThumb }
+                 onClick={ () => handleSubmit(Ingredient.idDrink) }
                />
              </li>
            )))}
--- src/services/apis.js
+++ src/services/apis.js
@@ -20,3 +20,9 @@ export const apiFirstLetter = async (firstLetter) => {
   const response = await request.json();
   return response;
 };
+
+export const SearchAPIidrevenue = async (id) => {
+  const request = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+  const response = await request.json();
+  return response;
+};
