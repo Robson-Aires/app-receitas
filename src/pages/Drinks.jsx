c5beb8d336ff4a1cd9c0c8275c10665b7f6792af
joao <joaopaz79@outlook.com>
2022-11-30 19:44:47 -0300

    Finaliza requisito 20.

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -4,19 +4,37 @@ import Header from '../components/Header';
 // import { useHistory } from 'react-router-dom';
 import recipesContext from '../context/recipesContext';
 import { apiIngredientes } from '../services/apiDrinks';
+import useFetch from '../hooks/useFetch';
 
 function Drinks() {
   const number12 = 12;
+  const numberOfButtons = 5;
   const { data, setData, setLoading, loading } = useContext(recipesContext);
   useEffect(() => {
     apiIngredientes()
       .then((result) => setData(result.drinks));
     setLoading(false);
   }, [setLoading, setData]);
+
+  const { fetchData, fetchLoading } = useFetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+
   if (loading) return <p>Carregando...</p>;
   return (
     <div>
       <Header title="Drinks" />
+      { fetchLoading ? <p>Carregando...</p> : (
+        fetchData.drinks.map((category, i) => (
+          i < numberOfButtons
+          && (
+            <button
+              key={ category.strCategory }
+              type="button"
+              data-testid={ `${category.strCategory}-category-filter` }
+            >
+              {category.strCategory}
+            </button>
+          )
+        )))}
       <ul>
         {data.map((Ingredient, index) => (
           index < number12
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -3,20 +3,37 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 // import { useHistory } from 'react-router-dom';
 import recipesContext from '../context/recipesContext';
+import useFetch from '../hooks/useFetch';
 import { apiIngredient } from '../services/apis';
 
 function Recipes() {
   const number12 = 12;
+  const numberOfButtons = 5;
   const { data, setData, setLoading, loading } = useContext(recipesContext);
   useEffect(() => {
     apiIngredient()
       .then((result) => setData(result.meals));
     setLoading(false);
   }, [setLoading, setData]);
+
+  const { fetchData, fetchLoading } = useFetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
   if (loading) return <p>Carregando...</p>;
   return (
     <div>
       <Header title="Meals" />
+      { fetchLoading ? <p>Carregando...</p> : (
+        fetchData.meals.map((category, i) => (
+          i < numberOfButtons
+          && (
+            <button
+              key={ category.strCategory }
+              type="button"
+              data-testid={ `${category.strCategory}-category-filter` }
+            >
+              {category.strCategory}
+            </button>
+          )
+        )))}
       <ul>
         {data.map((Ingredient, index) => (
           index < number12
