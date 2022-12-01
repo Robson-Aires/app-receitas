606de4607d6087d2160ce20f146d074d5cd1072e
joao <joaopaz79@outlook.com>
2022-11-30 23:17:10 -0300

    Resolve problemas de lint

--- src/context/recipesProvider.js
+++ src/context/recipesProvider.js
@@ -9,36 +9,35 @@ export default function RecipesProvider({ children }) {
   const [memoryData, setMemoryData] = useState([]);
   const [searchCategory, setSearchCategory] = useState('');
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleFilterButton = async (url, endpoint, recipe, category) => {
-    setLoading(true);
-    const request = await fetch(`${url}${endpoint}`);
-    const response = await request.json();
+  const values = useMemo(() => {
+    const handleFilterButton = async (url, endpoint, recipe, category) => {
+      setLoading(true);
+      const request = await fetch(`${url}${endpoint}`);
+      const response = await request.json();
 
-    if (category === searchCategory) {
-      setData(memoryData);
-    } else if (recipe === 'meals') {
-      setData(response.meals);
-    } else if (recipe === 'drinks') {
-      setData(response.drinks);
-    }
-    setLoading(false);
-  };
-
-  const values = useMemo(() => ({
-    data,
-    loading,
-    setData,
-    setLoading,
-    inputSearch,
-    setInputSearch,
-    radio,
-    setRadio,
-    handleFilterButton,
-    memoryData,
-    setMemoryData,
-    setSearchCategory,
-  }), [data, loading, inputSearch, radio, handleFilterButton, memoryData]);
+      if (category === searchCategory) {
+        setData(memoryData);
+      } else if (recipe === 'meals') {
+        setData(response.meals);
+      } else if (recipe === 'drinks') {
+        setData(response.drinks);
+      }
+      setLoading(false);
+    };
+    return {
+      data,
+      loading,
+      setData,
+      setLoading,
+      inputSearch,
+      setInputSearch,
+      radio,
+      setRadio,
+      handleFilterButton,
+      memoryData,
+      setMemoryData,
+      setSearchCategory };
+  }, [data, loading, inputSearch, radio, memoryData, searchCategory]);
 
   return (
     <recipesContext.Provider value={ values }>
