effa24836dd069bd7eaa947205e18af4d6595f5e
rachelsaronne <rachelsaronne@gmail.com>
2022-12-02 23:20:43 -0300

    test do req 39

---
+++ src/tests/RecipeInprogress.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../Helpers/renderWithRouter';
+
+describe('Testa a tela de RecipeInprogress, e os checkboxs', () => {
+  it('verifica os inputs e butoes', async () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => { history.push('/meals/52977'); });
+
+    const btnSearchTop = await screen.findByTestId('start-recipe-btn');
+    expect(btnSearchTop).toBeInTheDocument();
+
+    userEvent.click(btnSearchTop);
+
+    const inputName = await screen.findByTestId('1-ingredient-step');
+    expect(inputName).toBeInTheDocument();
+    userEvent.click(inputName);
+    userEvent.click(inputName);
+    // inputName = await screen.findByTestId('1-ingredient-step');
+    // expect(inputName).toBeChecked('checked');
+  });
+});
