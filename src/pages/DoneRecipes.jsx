4baff3c5e6fa5c17e3e57f03f8c327673c7065f5
Megas-MDN <mdn.melqui@gmail.com>
2022-12-04 02:13:56 -0300

    Finalizando requisitos 44 a 49

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,10 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+
 import Header from '../components/Header';
 
+const copy = require('clipboard-copy');
+
 function DoneRecipes() {
+  const [done, setDone] = useState([]);
+  const [message, setMessage] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    const local = JSON.parse((localStorage.getItem('doneRecipes') || '[]'));
+    setDone(local);
+  }, []);
+
+  const handleShare = (recipe) => {
+    setMessage(true);
+    const copied = `http://localhost:3000/${recipe.type}s/${recipe.id}`;
+    copy(copied);
+    console.log(copied);
+  };
+
+  const goFilter = (str) => {
+    const local = JSON.parse((localStorage.getItem('doneRecipes') || '[]'));
+    const arr = local.filter((el) => el.type.includes(str));
+    setDone(arr);
+  };
+
   return (
     <div>
       <Header title="Done Recipes" showIcon={ false } />
+      <button
+        data-testid="filter-by-all-btn"
+        type="button"
+        onClick={ () => goFilter('') }
+      >
+        All
+
+      </button>
+      <button
+        data-testid="filter-by-meal-btn"
+        type="button"
+        onClick={ () => goFilter('meal') }
+      >
+        Meals
+
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        type="button"
+        onClick={ () => goFilter('drink') }
+      >
+        Drinks
+
+      </button>
+      <div className="card-container">
+        {
+          done.map((el, i) => (
+            <div className="card" key={ el.id }>
+
+              <input
+                type="image"
+                src={ el.image }
+                alt=""
+                data-testid={ `${i}-horizontal-image` }
+                onClick={ () => history.push(`/${el.type}s/${el.id}`) }
+              />
+
+              <Link to={ `/${el.type}s/${el.id}` }>
+                <p data-testid={ `${i}-horizontal-name` }>{el.name}</p>
+              </Link>
+              <p
+                data-testid={ `${i}-horizontal-top-text` }
+              >
+                {(el.type === 'meal')
+                  ? `${el.nationality} - ${el.category}` : el.alcoholicOrNot }
+
+              </p>
+              <p data-testid={ `${i}-horizontal-done-date` }>{el.doneDate}</p>
+              {message && <p>Link copied!</p>}
+              <button
+                type="button"
+                onClick={ () => handleShare(el) }
+              >
+                <img
+                  src={ shareIcon }
+                  alt="compartilhar"
+                  data-testid={ `${i}-horizontal-share-btn` }
+                />
+              </button>
+              {el.tags.map((tag) => (
+                <p key={ tag } data-testid={ `${i}-${tag}-horizontal-tag` }>{tag}</p>
+              ))}
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ src/tests/DoneRecipes.test.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../Helpers/renderWithRouter';
+
+// const copy = require('clipboard-copy');
+
+const objDone = [{
+  id: '52977',
+  nationality: 'Turkish',
+  name: 'Corba',
+  category: 'Side',
+  image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+  tags: ['Soup'],
+  alcoholicOrNot: '',
+  type: 'meal',
+  doneDate: '2022-12-04T03:21:06.687Z' },
+{
+  id: '178319',
+  type: 'drink',
+  nationality: '',
+  category: 'Cocktail',
+  alcoholicOrNot: 'Alcoholic',
+  name: 'Aquamarine',
+  image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+  doneDate: '23/06/2020',
+  tags: [],
+},
+];
+
+const btnAll = 'filter-by-all-btn';
+const btnMeals = 'filter-by-meal-btn';
+const btnDrinks = 'filter-by-drink-btn';
+const btnShare = '0-horizontal-share-btn';
+const imag = '0-horizontal-image';
+const imag2 = '1-horizontal-image';
+
+const rota = '/done-recipes';
+
+describe('Testa Done recipes', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    localStorage.setItem('doneRecipes', JSON.stringify(objDone));
+    // jest.mock('clipboard-copy');
+  });
+  afterEach(() => { jest.resetAllMocks(); });
+  it('Clica em compartilhar', async () => {
+    // copy.mockReturnValueOnce(() => null);
+    // window.document.copy = jest.fn();
+    const { history } = renderWithRouter(<App />);
+
+    act(() => { history.push(rota); });
+
+    const goBtnShare = screen.getByTestId(btnShare);
+    expect(goBtnShare).toBeInTheDocument();
+
+    userEvent.click(goBtnShare);
+
+    // const inputName = screen.getByTestId(testidName);
+    // expect(inputName).toBeInTheDocument();
+
+    // const btnSearch = screen.getByTestId(testidBtnSearch);
+    // expect(btnSearch).toBeInTheDocument();
+
+    // userEvent.type(inputName, 'lime');
+    // jest.spyOn(global, 'fetch'); global.fetch.mockResolvedValue({ json: jest.fn().mockResolvedValue({ meals: null }) });
+    // userEvent.click(btnSearch);
+    await new Promise((r) => { setTimeout(r, 1000); });
+    jest.clearAllMocks();
+  });
+  it('Clica em filtro meals', async () => {
+    // copy.mockReturnValueOnce(() => null);
+    // window.document.copy = jest.fn();
+    const { history } = renderWithRouter(<App />);
+
+    act(() => { history.push(rota); });
+
+    const goBtnShare = screen.getByTestId(btnMeals);
+    expect(goBtnShare).toBeInTheDocument();
+
+    userEvent.click(goBtnShare);
+
+    // const inputName = screen.getByTestId(testidName);
+    // expect(inputName).toBeInTheDocument();
+
+    // const btnSearch = screen.getByTestId(testidBtnSearch);
+    // expect(btnSearch).toBeInTheDocument();
+
+    // userEvent.type(inputName, 'lime');
+    // jest.spyOn(global, 'fetch'); global.fetch.mockResolvedValue({ json: jest.fn().mockResolvedValue({ meals: null }) });
+    // userEvent.click(btnSearch);
+    await new Promise((r) => { setTimeout(r, 1000); });
+    jest.clearAllMocks();
+  });
+  it('Clica em drinks', async () => {
+    // copy.mockReturnValueOnce(() => null);
+    // window.document.copy = jest.fn();
+    const { history } = renderWithRouter(<App />);
+
+    act(() => { history.push(rota); });
+
+    const goBtnShare = screen.getByTestId(btnDrinks);
+    expect(goBtnShare).toBeInTheDocument();
+
+    userEvent.click(goBtnShare);
+
+    // const inputName = screen.getByTestId(testidName);
+    // expect(inputName).toBeInTheDocument();
+
+    // const btnSearch = screen.getByTestId(testidBtnSearch);
+    // expect(btnSearch).toBeInTheDocument();
+
+    // userEvent.type(inputName, 'lime');
+    // jest.spyOn(global, 'fetch'); global.fetch.mockResolvedValue({ json: jest.fn().mockResolvedValue({ meals: null }) });
+    // userEvent.click(btnSearch);
+    await new Promise((r) => { setTimeout(r, 1000); });
+    jest.clearAllMocks();
+  });
+  it('Clica em filtro all', async () => {
+    // copy.mockReturnValueOnce(() => null);
+    // window.document.copy = jest.fn();
+    const { history } = renderWithRouter(<App />);
+
+    act(() => { history.push(rota); });
+
+    const goBtnShare = screen.getByTestId(btnAll);
+    expect(goBtnShare).toBeInTheDocument();
+
+    userEvent.click(goBtnShare);
+
+    const inputName = screen.getByTestId(imag);
+    expect(inputName).toBeInTheDocument();
+    userEvent.click(inputName);
+
+    await new Promise((r) => { setTimeout(r, 1000); });
+    jest.clearAllMocks();
+  });
+  it('Clica em imagem drink', async () => {
+    // copy.mockReturnValueOnce(() => null);
+    // window.document.copy = jest.fn();
+    const { history } = renderWithRouter(<App />);
+
+    act(() => { history.push(rota); });
+
+    const inputName = screen.getByTestId(imag2);
+    expect(inputName).toBeInTheDocument();
+    userEvent.click(inputName);
+
+    console.log(history.location.pathname);
+    await new Promise((r) => { setTimeout(r, 1000); });
+    jest.clearAllMocks();
+  });
+});
+
+describe('Testa LocalStorage vazio', () => {
+  it('LocalStorage vazio', () => {
+    localStorage.clear();
+
+    const { history } = renderWithRouter(<App />);
+
+    act(() => { history.push(rota); });
+
+    const goBtnShare = screen.getByTestId(btnAll);
+    expect(goBtnShare).toBeInTheDocument();
+
+    userEvent.click(goBtnShare);
+
+    jest.clearAllMocks();
+  });
+});
